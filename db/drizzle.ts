2841bd5a49162adc702c6e64cbc5cd49dc14fcc9
virinci <revirinci@proton.me>
2024-12-04 00:38:19 +0530

    Test node-postgres with config

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,4 +3,14 @@ import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 config({ path: '.env.local' });
+
+/*
+export const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    // ssl: true
+  }
+}, { schema });
+*/
+
 export const db = drizzle(process.env.DATABASE_URL!, { schema });
