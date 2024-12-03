5b5ae82598f9a8e55f852a6c21d722b902322b7b
virinci <revirinci@proton.me>
2024-12-03 17:31:55 +0530

    Remove leftover from drizzle neon HTTP client

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,5 +1,4 @@
 import { config } from 'dotenv';
-import { neon } from '@neondatabase/serverless';
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 
