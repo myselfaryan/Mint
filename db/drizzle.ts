4976e3615e25bdc28c045bbf485187b13d3ca058
virinci <revirinci@proton.me>
2024-12-03 23:38:30 +0530

    Make the drizzle client schema aware

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,8 @@
 import { config } from 'dotenv';
 import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
+import * as schema from './schema';
 
 config({ path: '.env.local' });
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema });
