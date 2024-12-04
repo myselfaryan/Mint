e48ac5a797c671a8db80e830db42a80f5d47178a
virinci <revirinci@proton.me>
2024-12-04 16:36:44 +0530

    Remove the already extracted validation schemas

--- components/auth-component.tsx
+++ components/auth-component.tsx
@@ -28,30 +28,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-// const loginSchema = z.object({
-//   email: z.string().email({ message: "Invalid email address" }),
-//   password: z
-//     .string()
-//     .min(8, { message: "Password must be at least 8 characters long" }),
-// });
-
-// const registerSchema = loginSchema
-//   .extend({
-//     name: z
-//       .string()
-//       .min(2, { message: "Name must be at least 2 characters long" }),
-//     confirmPassword: z
-//       .string()
-//       .min(8, { message: "Password must be at least 8 characters long" }),
-//   })
-//   .refine((data) => data.password === data.confirmPassword, {
-//     message: "Passwords do not match",
-//     path: ["confirmPassword"],
-//   });
-
-// type LoginFormValues = z.infer<typeof loginSchema>;
-// type RegisterFormValues = z.infer<typeof registerSchema>;
-
 export function AuthComponent({
   initialMode = "login",
 }: {
