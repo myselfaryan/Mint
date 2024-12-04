280fdca144d3dd06697ac001ef60fe6336577503
aahnik <daw@aahnik.dev>
2024-12-04 17:32:11 +0530

    Fix issue with page freezing by hack
    https://github.com/shadcn-ui/ui/issues/544#issuecomment-1836353580

--- mint/generic-listing.tsx
+++ mint/generic-listing.tsx
@@ -216,7 +216,7 @@ export function GenericListing<T extends { id: number | string }>({
                   ))}
                   {(onEdit || onDelete) && (
                     <TableCell>
-                      <DropdownMenu>
+                      <DropdownMenu modal={false}>
                         <DropdownMenuTrigger asChild>
                           <Button variant="ghost" className="h-8 w-8 p-0">
                             <MoreHorizontal className="h-4 w-4" />
