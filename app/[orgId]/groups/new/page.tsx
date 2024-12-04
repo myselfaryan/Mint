4b66b3f29ab68eaf7a876890a014119e203acf57
aahnik <daw@aahnik.dev>
2024-12-05 03:24:21 +0530

    Remove redundant new/create components and pages for contests, groups, and users

--- app/[orgId]/groups/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import { GroupCreationPage } from "@/components/group-creation-page";
-
-export default function Component() {
-  return <GroupCreationPage />;
-}
--- app/[orgId]/users/new/page.tsx
+++
@@ -1,6 +0,0 @@
-// import CreateUserPage from "@/components/newuser-add";
-
-export default function UsersPage() {
-  // return <CreateUserPage />;
-  return <h1>New User</h1>;
-}
