5d5c37e77ccbf4c4d14bcf52af06b3e273d97a3f
virinci <revirinci@proton.me>
2025-01-10 04:56:25 +0530

    Use problem code for referring to the test cases instead of its ID

--- app/api/orgs/[orgId]/problems/[problemId]/test-cases/route.ts
+++ app/api/orgs/[orgId]/problems/[problemId]/test-cases/route.ts
@@ -1,18 +1,27 @@
 import { NextRequest } from "next/server";
 import { createTestCaseSchema } from "@/lib/validations";
 import * as testCaseService from "./service";
-import { IdSchema } from "@/app/api/types";
+import { NameIdSchema } from "@/app/api/types";
+import { getOrgIdFromNameId } from "@/app/api/service";
 import { z } from "zod";
 
 export async function GET(
   _req: NextRequest,
-  { params }: { params: { problemId: string } },
+  { params }: { params: { orgId: string; problemId: string } },
 ) {
   try {
-    const problemId = IdSchema.parse(params.problemId);
-    const testCases = await testCaseService.getTestCases(problemId);
+    const orgId = await getOrgIdFromNameId(NameIdSchema.parse(params.orgId));
+    const problemCode = NameIdSchema.parse(params.problemId);
+    console.log(params.orgId, orgId, params.problemId, problemCode);
+    const testCases = await testCaseService.getTestCases(orgId, problemCode);
     return Response.json(testCases);
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return Response.json({ error: error.errors }, { status: 400 });
+    }
+    if (error instanceof Error && error.message === "Problem not found") {
+      return Response.json({ error: error.message }, { status: 404 });
+    }
     return Response.json(
       { error: "Failed to fetch test cases" },
       { status: 500 },
@@ -22,17 +31,21 @@ export async function GET(
 
 export async function POST(
   request: NextRequest,
-  { params }: { params: { problemId: string } },
+  { params }: { params: { orgId: string; problemId: string } },
 ) {
   try {
-    const problemId = IdSchema.parse(params.problemId);
+    const orgId = await getOrgIdFromNameId(NameIdSchema.parse(params.orgId));
+    const problemCode = NameIdSchema.parse(params.problemId);
     const data = createTestCaseSchema.parse(await request.json());
 
-    const testCase = await testCaseService.addTestCase(problemId, data);
+    const testCase = await testCaseService.addTestCase(orgId, problemCode, data);
     return Response.json(testCase, { status: 201 });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return Response.json({ error: error.errors }, { status: 400 });
+    }
+    if (error instanceof Error && error.message === "Problem not found") {
+      return Response.json({ error: error.message }, { status: 404 });
     }
     return Response.json(
       { error: "Failed to create test case" },
