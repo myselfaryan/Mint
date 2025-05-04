f028f7355a7c28fb2bce01246f11b5e117ebec14
aahnik <daw@aahnik.dev>
2025-05-05 00:11:31 +0530

    Fix api-doc/page.tsx

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,13 +1,12 @@
-import { getApiDocs } from "@/lib/swagger";
+import { apiDocument } from "@/lib/swagger";
 import ReactSwagger from "./react-swagger";
 import { Suspense } from "react";
 
 export default async function ApiDoc() {
-  const spec = await getApiDocs();
   return (
     <section className="container mx-auto p-4">
       <Suspense fallback={<div>Loading API documentation...</div>}>
-        <ReactSwagger spec={spec} />
+        <ReactSwagger spec={apiDocument} />
       </Suspense>
     </section>
   );
